6a525bab6d0670c7c0ab237fe24e758abbdae350
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-09-04 12:23:54 +0530

    Feat: Added Folder Structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar, Feed, VideoDetail, SearchFeed, ChannellDetail } from "./components/index";
+
+
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetail />} />
+                <Route path="/channel/:id" element={<ChannellDetail />} />
+                <Route path="/search/:seachTerm" element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
+
+export default App;
---
+++ src/components/ChannellDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChannellDetail = () => {
+    return (
+        <div>ChannellDetail</div>
+    )
+}
+
+export default ChannellDetail;
