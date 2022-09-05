2c89bc774e32de00e9b2b0cdcf317dff1e61d672
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-09-05 09:30:09 +0530

    Fixed: Fixed flex-start css issue

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -10,8 +10,8 @@ const Videos = ({ videos, direction }) => {
             <Stack
                 direction={direction || "row"}
                 flexWrap="wrap"
-                justifyContent="start"
-                alignItems="start"
+                justifyContent="flex-start"
+                alignItems="flex-start"
                 gap={2}
             >
                 {
